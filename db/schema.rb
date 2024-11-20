b85052c44cf206e57a79e0ad68445384caf85171
crisdj13-code <cristoferj981@gmail.com>
2024-11-20 04:04:47 -0500

    added comments and mailers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_19_071326) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_20_054605) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -61,6 +61,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_19_071326) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -70,4 +78,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_19_071326) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
 end
