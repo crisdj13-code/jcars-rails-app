bbb328f5968ec79902ec9ae8294dbe80dd235f04
crisdj13-code <cristoferj981@gmail.com>
2024-11-21 22:12:24 -0500

    live broadcasting effect is not fixed with DelayedJobs

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module Jcars
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.active_job.queue_adapter = :async # or :sidekiq, :resque, etc.
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
