bc9f80ec929c2873dc4927dad04167e3c9ad0fae
crisdj13-code <cristoferj981@gmail.com>
2024-12-06 08:20:34 -0500

    tryind to debug redis error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,1 @@
-Redis.current = Redis.new(
-  url: ENV['REDIS_URL'],  # URL should already include everything, including SSL and password
-  ssl: true,
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-)
+$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
