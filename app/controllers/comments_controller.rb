addaaccb53b70b4a7ced2476ab91ff7e17005d4b
crisdj13-code <cristoferj981@gmail.com>
2024-12-06 06:58:45 -0500

    tryind to debug redis error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,9 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
+    puts "Params: #{params.inspect}"
     puts "***#############********************** Current User Info: #{current_user.inspect} ***##############**********************&&&****" #debugging to see if current user is set 
-    @comment = @post.comments.create! params.required(:comment).permit(:content)
+    @comment = @post.comments.create!(comment_params)
     if @comment.save
       # mailer is not coonfigured yet
       #CommentsMailer.submitted(@comment).deliver_later
@@ -12,9 +13,11 @@ class CommentsController < ApplicationController
     else 
     end
   end
-
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
   private
     def set_post
-      @post = Post.find(params[:post_id])
+      @post = Post.find(params[:post_id].to_i)
     end
 end
