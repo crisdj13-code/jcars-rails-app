7000be657cad6e73befac4a7368182b29c9269fb
crisdj13-code <cristoferj981@gmail.com>
2024-12-06 05:19:36 -0500

    updated css and background images as well as converted a few button_to to link_to in order to properly request routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  #trying to fix css not updateing
+  config.assets.compile = true
+  config.assets.debug = true
+  config.assets.digest = false 
+  config.assets.check_precompiled_asset = false
+  
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
